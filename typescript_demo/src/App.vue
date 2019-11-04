3a2f1bfc93ac1cf423e681b1803e3b0662585ea7
Jeff Daze <jdaze@hostopia.com>
2019-11-03 23:27:34 -0500

    Quick test to ensure my build pipeline is functional

--- typescript_demo/src/App.vue
+++ typescript_demo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Now to ensure the build proess works and validate the end result..."/>
   </div>
 </template>
 
