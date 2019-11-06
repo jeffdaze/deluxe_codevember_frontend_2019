ca4d98c71825f6630ea7a4b044640281617bbf57
Jeff Daze <jdaze@hostopia.com>
2019-11-06 16:25:05 -0500

    Added initial styles and transitions for Card Display

--- typescript_demo/src/components/CreditCard.vue
+++ typescript_demo/src/components/CreditCard.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="cardForm">
+<div class="mainView">
+
+<div id="cardDisplay" :class="{'tiltView': focusSet}">CARD TEST</div>
+
+  <div class="cardForm" :class="{'moveDown': focusSet}">
     <div class="formGroup">
         <div class="fieldLabel">Card Number:</div>
-        <div><input type="number" class="formInput"></div>
+        <div><input type="number" class="formInput" @focus="gotFocus()"></div>
     </div>
     <div class="formGroup">
         <div class="fieldLabel">Cardholder Name:</div>
-        <div><input type="text" class="formInput"></div>
+        <div><input type="text" class="formInput" @focus="gotFocus()"></div>
     </div>
         <div>
             <div class="formSplit">
                 <div class="formGroup">
                     <div class="fieldLabel">Expiration:</div>
-                    <select>
+                    <select @focus="gotFocus()">
                         <option value="" disabled selected>Month</option>
                         <option></option>
                     </select>
-                    <select>
+                    <select @focus="gotFocus()">
                         <option value="" disabled selected>Year</option>
                         <option></option>
                     </select>
@@ -25,12 +29,14 @@
             <div class="formSplit">
                 <div class="formGroup">
                     <div class="fieldLabel">CVV:</div>
-                    <input type="number" class="formInput">
+                    <input type="number" class="formInput" @focus="gotFocus()">
                 </div>
             </div>
         </div>
 
       <button @click="test2()">Submit</button>
+  </div>
+
   </div>
 </template>
 
@@ -41,6 +47,8 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 export default class CreditCard extends Vue {
   someVal: string = 'test'
   fromForm: string = ''
+  focusSet: boolean = false;
+
   test (): void{
     console.log('Ran a test method...')
   }
@@ -49,6 +57,9 @@ export default class CreditCard extends Vue {
     this.someVal = this.fromForm
     // now clear the form...
     this.fromForm = ''
+  }
+  gotFocus (): void{
+    this.focusSet = true
   }
 }
 </script>
@@ -67,17 +78,37 @@ SELECT {
     float: left;
 }
 
+.mainView {
+    perspective: 600px;
+}
+
+#cardDisplay {
+    top: 10px;
+    right:300px;
+    position: absolute;
+    border:2px solid #000;
+    border-radius: 25px;
+    width: 300px;
+    height: 200px;
+    background: #e66c6c;
+    transition: all 0.8s;
+}
+
 .formGroup {
     padding: 10px;
     width: 100%;
 }
 
 .cardForm {
-    border:1px solid #000;
+    background: #7ebcf7;
+    position: absolute;
+    display: block;
+    border:2px solid #000;
     border-radius: 25px;
     width: 600px;
     margin: 0 auto;
     padding: 30px;
+    transition: all 0.5s;
 }
 
 .fieldLabel {
@@ -92,6 +123,18 @@ SELECT {
 .formSplit {
     display: inline-flex;
     width: 50%;
+}
+
+.moveDown {
+    margin-top: 100px;
+}
+
+.tiltView {
+    top: -50px !important;
+    right: 10px !important;
+    transform-origin: 100% 70%;
+    transform: rotateY(-15deg);
+    z-index:1000;
 }
 
 h3 {
