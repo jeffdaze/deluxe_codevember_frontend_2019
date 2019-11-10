2a481946347735f3a3785a164f4173154b8baedb
Jeff Daze <jdaze@hostopia.com>
2019-11-09 23:18:45 -0500

    Added date values

--- typescript_demo/src/components/CreditCard.vue
+++ typescript_demo/src/components/CreditCard.vue
@@ -20,11 +20,32 @@
                     <div class="fieldLabel">Expiration:</div>
                     <select @focus="gotFocus()" @blur="lostFocus()">
                         <option value="" disabled selected>Month</option>
-                        <option></option>
+                        <option value="01">January</option>
+                        <option value="02">February</option>
+                        <option value="03">March</option>
+                        <option value="04">April</option>
+                        <option value="05">May</option>
+                        <option value="06">June</option>
+                        <option value="07">July</option>
+                        <option value="08">August</option>
+                        <option value="09">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
                     </select>
                     <select @focus="gotFocus()" @blur="lostFocus()">
                         <option value="" disabled selected>Year</option>
-                        <option></option>
+                        <option value="2019">2019</option>
+                        <option value="2020">2020</option>
+                        <option value="2021">2021</option>
+                        <option value="2022">2022</option>
+                        <option value="2023">2023</option>
+                        <option value="2024">2024</option>
+                        <option value="2025">2025</option>
+                        <option value="2026">2026</option>
+                        <option value="2027">2027</option>
+                        <option value="2028">2028</option>
+                        <option value="2029">2029</option>
                     </select>
                 </div>
             </div>
@@ -51,6 +72,7 @@ export default class CreditCard extends Vue {
   fromForm: string = ''
   focusSet: boolean = false;
   ccValue: number | null = null;
+  ccCvv: number | null = null;
   ccName: string = '';
   test (): void{
     console.log('Ran a test method...')
