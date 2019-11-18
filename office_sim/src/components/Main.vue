c7d2dab1ece19f5ea415b9ed7a5ac23efa24fcb1
Jeff Daze <jdaze@hostopia.com>
2019-11-18 00:29:41 -0500

    Scaffold vuex store and test methods

---
+++ office_sim/src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main">
+    <button>Get Job ${{ jobBid }}</button>
+    <button @click="addFunds(10)">Add funds $10</button>
+
+    <div class="currentFunds">Current Funds: {{ this.$store.getters.getFunds }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Main extends Vue {
+  /* @Prop() private msg!: string; */
+    jobBid: number = 0;
+
+    addFunds(amt: number): void{
+        this.$store.commit('addFunds', amt);
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- office_sim/src/store/index.ts
+++ office_sim/src/store/index.ts
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    funds: 0
+  },
+  mutations: {
+    addFunds (state: any, amount: number): void {
+      state.funds += amount;
+    },
+    deleteFunds (state: any, amount: number): void {
+      state.funds -= amount;
+    }
+  },
+  actions: {
+
+  },
+  getters: {
+    getFunds (state: any): number {
+      return state.funds;
+    }
+  }
 });
