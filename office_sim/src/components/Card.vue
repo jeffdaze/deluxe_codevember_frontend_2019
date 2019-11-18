b3e9ca560b1ee3534183695849fcb0e6297b34aa
Jeff Daze <jdaze@hostopia.com>
2019-11-18 17:58:57 -0500

    Card component and test Layout

---
+++ office_sim/src/components/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <img :src="icon" />
+    <div class="projectName">{{ projectName }}</div>
+    <div class="projectState">{{ projectState }}</div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Card extends Vue {
+  /* @Prop() private msg!: string; */
+    projectName: string = "Test Project";
+    // probably put this in the vuex store...
+    projectState: string = "Planning";
+    icon: string = "";
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+    font-size: 10px;
+    border:1px solid #333333;
+    border-radius: 25%;
+    width:70px;
+    height:70px;
+    float:left;
+    margin:12px;
+}
+
+
+</style>
--- office_sim/src/components/Main.vue
+++ office_sim/src/components/Main.vue
@@ -4,21 +4,49 @@
     <button @click="addFunds(10)">Add funds $10</button>
 
     <div class="currentFunds">Current Funds: {{ this.$store.getters.getFunds }}</div>
+
+    <div id="jobBoard" class="boardCell">
+        Project Listings
+
+        <div v-for="x in cardSet" v-bind:key="x">
+            <Card />
+        </div>
+
+    </div>
+    <div id="planning" class="boardCell">
+        Planning
+    </div>
+    <div id="coding" class="boardCell">
+        Coding
+    </div>
+    <div id="done" class="boardCell">
+        Done
+    </div>
+
+    
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import Card from "@/components/Card.vue";
 
-@Component
+@Component({
+    components: {
+        Card
+    }
+})
 export default class Main extends Vue {
   /* @Prop() private msg!: string; */
     jobBid: number = 0;
-
+    cardSet: number = 8;
     addFunds(amt: number): void{
         this.$store.commit('addFunds', amt);
     }
 }
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -37,4 +65,14 @@ li {
 a {
   color: #42b983;
 }
+
+.boardCell {
+    border: 1px solid #000000;
+    width:210px;
+    height:320px;
+    margin-right:20px;
+    display:inline-block;
+    overflow: auto;
+}
+
 </style>
