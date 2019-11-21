77ddf088939275208743914717ce1a4992e79916
Jeff Daze <jdaze@hostopia.com>
2019-11-21 15:58:46 -0500

    Quick code cleanup

--- office_sim/src/components/Card.vue
+++ office_sim/src/components/Card.vue
@@ -4,7 +4,6 @@
     <div class="projectName">{{ projectName }}</div>
     <div class="projectState">{{ projectState }}</div>
     <div class="challenge">{{ challenge }}</div>
-
   </div>
 </template>
 
@@ -13,9 +12,9 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class Card extends Vue {
-  @Prop() private projectName: string;
+  @Prop() private projectName!: string;
   @Prop() private projectState!: string;
-  @Prop() private icon: string;
+  @Prop() private icon!: string;
   @Prop() private challenge!: number;
 }
 </script>
@@ -23,14 +22,12 @@ export default class Card extends Vue {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .card {
-    font-size: 10px;
-    border:1px solid #333333;
-    border-radius: 25%;
-    width:70px;
-    height:70px;
-    float:left;
-    margin:12px;
+  font-size: 10px;
+  border: 1px solid #333333;
+  border-radius: 25%;
+  width: 70px;
+  height: 70px;
+  float: left;
+  margin: 12px;
 }
-
-
 </style>
--- office_sim/src/components/Progress.vue
+++ office_sim/src/components/Progress.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="progress">
-      <div class="bar" role="progressbar" :style="{width:getPercent+'%'}">{{ eventLabel }}</div>
+    <div class="bar" role="progressbar" :style="{ width: getPercent + '%' }">
+      {{ eventLabel }}
+    </div>
   </div>
 </template>
 
@@ -9,30 +11,29 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
 export default class Card extends Vue {
-  @Prop() private eventLabel!: string; 
-  @Prop() private total: number;
-  @Prop() private currentVal: number;
+  @Prop() private eventLabel!: string;
+  @Prop() private total!: number;
+  @Prop() private currentVal!: number;
 
-    get getPercent(): number {
-            return (this.currentVal / this.total * 100)
-    }
-
+  get getPercent(): number {
+    return (this.currentVal / this.total) * 100;
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .progress {
-    width:100%;
-    height: 20px;
-    background: #eaeaea;
-    color: #FFFFFF;
+  width: 100%;
+  height: 20px;
+  background: #eaeaea;
+  color: #ffffff;
 }
 
 .bar {
-    background:#008800;
-    height:100%;
-    line-height: 20px;
-    transition: all 0.5s;
+  background: #008800;
+  height: 100%;
+  line-height: 20px;
+  transition: all 0.5s;
 }
 </style>
